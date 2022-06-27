5af4e04d8de693c77d9441afa028de61df7198de
Hemankita Perabathini <phemankita@gmail.com>
2022-06-27 18:29:45 +0530

    fixes
    Signed-off-by: Hemankita Perabathini <phemankita@gmail.com>

--- kustomize/components/scripts/start-mqsc.sh
+++ kustomize/components/scripts/start-mqsc.sh
@@ -6,13 +6,13 @@ ckksum=""
 # Outer loop that keeps the MQ service running
 while true; do
 
-   tmpCksum=`cksum /dyn-mq-config-mqsc/dynamic-qm.mqsc | cut -d" " -f1`
+   tmpCksum=`cksum /dynamic-mq-config-mqsc/dynamic-definitions.mqsc | cut -d" " -f1`
 
    if (( tmpCksum != cksum ))
    then
       cksum=$tmpCksum
       echo "Applying MQSC"
-      runmqsc $1 < /dyn-mq-config-mqsc/dynamic-qm.mqsc
+      runmqsc $1 < /dynamic-mq-config-mqsc/dynamic-definitions.mqsc
    else
       sleep 3
    fi
